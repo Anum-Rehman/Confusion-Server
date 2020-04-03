14e0245158e0218f22ced95dc01c9ca029d7bbd8
Anum-Rehman <anumr32@gmail.com>
2020-04-04 01:36:26 +0500

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,9 +17,9 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/Fusion';
 const connect = mongoose.connect(url);
 
-connect.then((db)=>{
+connect.then((db) => {
   console.log('Connected correctly to the server');
-},(err)=>{ console.log(err); })
+}, (err) => { console.log(err); })
 
 var app = express();
 
@@ -28,32 +30,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321')); //Secret Key
 
-function auth(req,res,next){
-  console.log(req.headers);
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
 
-  var authHeader = req.headers.authorization
-  if(!authHeader){
-    var err = new Error('You are not authenticated!');
-    res.setHeader('www-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+function auth(req, res, next) {
+  console.log(req.session);
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('www-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+    //Buffer allows us to split value and to encode it
+    //Buffer.from is added on new verions of nodeJs for security
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':') //the second split is to split username and password
+
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username === 'admin' && password === 'password') {
+      req.session.user = 'admin'
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('www-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
-  //Buffer allows us to split value and to encode it
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':') //the second split is to split username and password
-
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password === 'password'){
-    next();
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('www-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+  else {
+    if (req.session.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
@@ -63,17 +86,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
