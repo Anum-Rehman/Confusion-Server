3d9c8953bb040875c918c54d9a4df00a6edce4ec
Anum-Rehman <anumr32@gmail.com>
2020-04-12 02:09:58 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/Fusion'
+    'mongoUrl' : 'mongodb://localhost:27017/Fusion',
+    'facebook': {
+        clientId: '3513473718679241',
+        clientSecret: 'c24965598bdca8a0523a411dc36f1768'
+    }
 }
